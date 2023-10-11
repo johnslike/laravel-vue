d6e214dd5a1e99cb0c239847c07ff23a8e776bf8
johnslike <vmacionjr@gmail.com>
2023-10-11 14:40:44 +0800

    first commit

---
+++ resources/js/pages/users/ListUsers.vue
@@ -0,0 +1,62 @@
+<script setup>
+const users = [
+    {   
+    id: 1,
+    name: 'John Doe',
+    email: 'john@gmail.com'
+},
+{   
+    id: 2,
+    name: 'Victor Doe',
+    email: 'victor@gmail.com'
+}
+];
+</script>
+
+<template>
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                <h1 class="m-0">Users</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">Users</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="content">
+        <div class="container-fluid">
+            <div class="card">
+                <div class="card-body">
+                    <table class="table table table-bordered">
+                        <thead>
+                            <tr>
+                                <th style="width: 10px;">#</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Registered Date</th>
+                                <th>Role</th>
+                                <th>Options</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="user in users" :key="user.id">
+                                <td>{{ user.id }}</td>
+                                <td>{{ user.name }}</td>
+                                <td>{{ user.email }}</td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
